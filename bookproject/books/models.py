e64b9537151c3067b4972e777a29903200fac40c
Hudanurediz <hudanurediz29@gmail.com>
2023-06-21 14:38:17 +0300

    new project

---
+++ bookproject/books/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.http import HttpResponse
+# Create your models here.
+
+class Author(models.Model):
+    name=models.CharField(max_length=100)
+    created=models.DateTimeField('created')
+
+class Book(models.Model):
+    name=models.CharField(max_length=100)
+    created=models.DateTimeField('created')
+    author=models.ForeignKey(Author,on_delete=models.CASCADE)
+    price=models.DecimalField(decimal_places=3, max_digits=6,null=True)
+    release_date=models.DateField(max_length=4,null=True)
+
